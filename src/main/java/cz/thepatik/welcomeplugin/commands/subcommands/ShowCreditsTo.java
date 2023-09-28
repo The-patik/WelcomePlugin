752303c74a53e4e23114ea959fc8e34684186763
The-patik <velechovskypatrik@gmail.com>
2023-09-28 21:30:47 +0200

    Prepare for version 0.4-alpha!
    New features - CommandManager, custom titles and more!

---
+++ src/main/java/cz/thepatik/welcomeplugin/commands/subcommands/ShowCreditsTo.java
@@ -0,0 +1,32 @@
+package cz.thepatik.welcomeplugin.commands.subcommands;
+
+import cz.thepatik.welcomeplugin.commands.SubCommand;
+import org.bukkit.entity.Player;
+
+public class ShowCreditsTo extends SubCommand {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getPermissions() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getSyntax() {
+        return null;
+    }
+
+    @Override
+    public void perform(Player player, String[] args) {
+
+    }
+}
+
